0d3afd876e24b1a5c360ee2712545e70fb553c83
Lanny Rivest <lanny.rivest@gmail.com>
2020-07-23 23:16:14 +0000

    Icecream and packing-lists

--- ice-cream.js
+++ ice-cream.js
@@ -5,5 +5,7 @@ console.log(iceCreamFlavours);
 // Your code below here...
 iceCreamFlavours.push('root beer');
 console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
 
-console.log(5+'3')
